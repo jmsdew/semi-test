2488a1c569964287d0d23400e0dd2bcb6b7a0288
jmsdew <jmsdew@naver.com>
2023-12-14 16:01:56 +0900

    first commit

---
+++ semi-study/src/main/java/com/ohgiraffers/semistudy/SemiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.semistudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemiStudyApplication.class, args);
+	}
+
+}
